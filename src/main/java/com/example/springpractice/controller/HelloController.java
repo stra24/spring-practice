bcc32fe2e60fe0582f67f4a1fee9d88f639788f6
junji <test@example.com>
2026-05-12 00:46:09 +0900

    【第2章-2】DIについて理解しよう（Beanの注入）

--- src/main/java/com/example/springpractice/controller/HelloController.java
+++ src/main/java/com/example/springpractice/controller/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.springpractice.controller;
 
+import com.example.springpractice.service.HelloService;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+  private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+  private final HelloService helloService;
+
+  @Autowired
+  public HelloController(HelloService helloService) {
+    this.helloService = helloService;
+    logger.info("HelloController の Bean を作成してDIコンテナの中に入れます");
+  }
+
   @GetMapping("/api/hello")
   public Map<String, String> hello() {
-    return Map.of("message", "Hello, Spring Boot!");
+    String greetMessage = helloService.greet("太郎");
+    return Map.of("greetMessage", greetMessage);
   }
 }
