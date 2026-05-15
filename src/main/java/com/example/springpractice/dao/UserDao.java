40fa897cfc5b0b7c66010e912e0f1aba99d1cc30
junji <test@example.com>
2026-05-16 02:28:21 +0900

    【第3章-3】派生クエリメソッドで一覧取得APIを改善しよう

--- src/main/java/com/example/springpractice/dao/UserDao.java
+++ src/main/java/com/example/springpractice/dao/UserDao.java
@@ -1,8 +1,10 @@
 package com.example.springpractice.dao;
 
 import com.example.springpractice.entity.User;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserDao extends CrudRepository<User, Long> {
 
+  List<User> findAllByOrderByIdAsc();
 }
--- src/main/java/com/example/springpractice/repository/UserRepository.java
+++ src/main/java/com/example/springpractice/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.springpractice.repository;
 
 import com.example.springpractice.dao.UserDao;
 import com.example.springpractice.entity.User;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -14,8 +13,6 @@ public class UserRepository {
   private final UserDao userDao;
 
   public List<User> findAll() {
-    List<User> list = new ArrayList<>();
-    userDao.findAll().forEach(list::add);
-    return list;
+    return userDao.findAllByOrderByIdAsc();
   }
 }
